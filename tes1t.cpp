c3c185ad605d45bec8dfe6cf02e49522dbd8d0d1
Gaurav suryavanshi <80798531+gauravsuryvanshi06@users.noreply.github.com>
2022-09-03 00:00:00 +0530

    Add files via upload

---
+++ tes1t.cpp
@@ -0,0 +1,18 @@
+#codeforces
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        string s[3][n];
+        for(int i=0;i<3;i++){
+            for(int j=0;j<n;j++){
+                cin>>s[i][j];
+            }
+        }
+        map<string,int>m;
+        
+    }
+}
